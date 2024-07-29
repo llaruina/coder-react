fa93e173a31dc8c1b9442225d30f6ba5e46f86e9
Luis Laruina <simpluis2@gmail.com>
2024-07-28 21:09:24 -0300

    Arreglos

--- src/componentes/ItemDetailConteiner/ItemDetailConteiner.jsx
+++ src/componentes/ItemDetailConteiner/ItemDetailConteiner.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getProductoPorId, getProductos } from '../../../mocks/asyncMock'
+import ItemDetail from '../ItemDetail/ItemDetail'
 
 export const ItemDetailContainer = () => {
 
     const [producto, setProducto] = useState({})
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useSate()
+    const [error, setError] = useState()
     const [idProducto, setIdProducto] = useState(1)
     const [mensaje, setMensaje] = useState('')
 
 
     useEffect(() => {
-        getProductoPorId()
+        getProductoPorId(1).then(res => setProducto(res)).finally(() => setLoading(false))
     }, [idProducto])
 
+    if (loading) {
+        return (
+            <h1> Cargando ... </h1>
+        )
+    }
 
 
     return (
